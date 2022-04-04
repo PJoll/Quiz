959be0bf47136e1c6146e5b1e7981c0adf26ca5f
Peter Jolley <peterjolley456@gmail.com>
2022-04-04 11:46:18 +0930

    Readme added questions added bugs fixed

---
+++ README.md
@@ -0,0 +1,33 @@
+# Quiz
+
+## Description 
+The goal of the project is to complete a  timed quiz on JavaScript completing several requirements.
+
+**The following problems are solved,**
+
+-The button "Star Quiz" commences  the quiz and a timer immideately begins
+
+-A question then is asked and answered. If incorrect time is deducted from the timer.
+
+-Once the quiz has completed you can store your initials and score
+
+
+**Throught the project i learnt the following,**
+
+-Using event listners
+
+-Performing get element commands
+
+-Enhancing the powerful use of Arrays and strings
+
+-Trying to use local storage
+
+
+## Usage 
+The following link is the completed active website.
+https://pjoll.github.io/Quiz/
+
+The following image shows the completed result.
+
+
+![alt text](/assets/Final%20Quiz.png)
--- assets/script.js
+++ assets/script.js
@@ -9,25 +9,54 @@ var myQuestions = [
     {
         question: "What is the use of an array?",
         answers: {
-            a: "asdfasd",
-            b: "hjkhjk",
-            c: "tyutu",
-            d: "456456",
+            a: "To only store numbers",
+            b: "To hide elements",
+            c: "To store a fixed sized collection of the same data type",
+            d: "All Of the Above",
         },
         correctAnswer: "c",
         userAnswer: "-1",
     },
     {
-        question: "What does 'DOM' mean? ",
+        question: "How do you find the highest number of x and y? ",
         answers: {
-            a: "zcfasdf",
-            b: "rtyrty",
-            c: "yuiyui",
-            d: "piup",
+            a: "max(x,y)",
+            b: "top(x,y)",
+            c: "highest.value(x,y) ",
+            d: "Math.max(x,y)",
         },
         correctAnswer: "d",
         userAnswer: "-1",
-    }
+    },
+    {
+        question: "Java is the same as Java Scipt? ",
+        answers: {
+            a: "False",
+            b: "True",
+        },
+        correctAnswer: "a",
+        userAnswer: "-1",
+    },
+    {
+        question: "How do you declare a Java Script variable? ",
+        answers: {
+            a: "variable catBreed",
+            b: "for cat-Breed",
+            c: "var catBreed ",
+            d: "var (catBreed)",
+        },
+        correctAnswer: "c",
+        userAnswer: "-1",
+    },
+    {
+        question: "The external JavaScript file must contain the <script> tag? ",
+        answers: {
+            a: "False",
+            b: "True",
+        },
+        correctAnswer: "a",
+        userAnswer: "-1",
+    },
 ]
 var timeLeft = 30
 var timer
@@ -95,63 +124,23 @@ function quizEnded() {
     document.getElementById("results").style.display = "block"
 
 }
-function displayHighscores(){
-var initials = document.getElementById("initials-input").value 
-document.getElementById("results").style.display = "none"
+function displayHighscores() {
+    var initials = document.getElementById("initials-input").value
+    document.getElementById("results").style.display = "none"
+    localStorage.setItem("results", JSON.stringify(initials));
+    renderMessage();
+    localStorage.setItem("results", JSON.stringify(initials));
 }
-//store to local storage
 startButton.addEventListener("click", function () {
     startButton.style.display = "none"
     updateTime()
     renderQuestion()
 })
 document.getElementById("Submit-Quiz", displayHighscores)
-// function generateQuiz() {
 
-//     function showQuestions(questions, quizContainer) {
-//         var output = [];
-//         var answers = {};
-//         for (var i = 0; i < questions.length; i++) {
-//             answers = [];
-//             for (letter in questions[i].answers) {
-//                 answers.push(
-//                     '<label>'
-//                     + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-//                     + letter + ': '
-//                     + questions[i].answers[letter]
-//                     + '</label>'
-//                 );
-//             }
-//             output.push(
-//                 '<div class="question">' + questions[i].question + '</div>'
-//                 + '<div class="answers">' + answers.join('') + '</div>'
-//             );
-//         }
-//         quizContainer.innerHTML = output.join('')
-//     }
-//     showQuestions(questions, quizContainer);
-//     function showResults(questions, quizContainer, resultsContainer) {
-
-//         var answersContainers = quizContainer.querySelectorAll('answers');
-//         var userAnswer = "";
-//         var numCorrect = 0;
-//         for (var i = 0; i < questions.length; i++) {
-//             userAnswer = (answersContainers[i].querySelector("input[name=question'+i'):checked") || {}).value;
-//             if (userAnswer === questions[i], correctAnswer) {
-//                 numCorrect++;
-//             }
-//         }
-//         resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-//     }
-
-//     // show the questions
-
-
-//     submitButton.onclick = function () {
-//         showResults(questions, quizContainer, resultsContainer);
-//     }
-// }
-// var quizContainer = document.getElementById('quiz');
-// var resultsContainer = document.getElementById('results');
-// var submitButton = document.getElementById('submit');
-// generateQuiz();
+submitButton.addEventListener("click", function () {
+    submitButton.style.display = "none"
+    document.getElementById("results").style.display = "block"
+    clearTimeout()
+    renderQuestion()
+})
